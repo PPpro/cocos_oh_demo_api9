1753c004a39ab2f69199ae94b586b8998361c090
pp_pro <chinese1236@qq.com>
2022-07-25 21:11:40 +0800

    update install bat

--- install_run.bat
+++ install_run.bat
@@ -1,9 +1,7 @@
-set tool=D:\openharmony_SDK\toolchains\3.2.2.5\hdc_std.exe
 set pack_local_path=C:\Users\pp\Desktop\cocos_oh_demo_api9\entry\build\default\outputs\default\entry-default-signed.hap
 
-%tool% uninstall ohos.example.myapplication
-%tool% install -r %pack_local_path%
+hdc_std uninstall ohos.example.myapplication
+hdc_std install -r %pack_local_path%
 
-%tool% shell aa start -a com.example.entry.MainAbility  -b  ohos.example.myapplication
-
-%tool% hilog -r > ./log.txt
+hdc_std shell aa start -a com.example.entry.MainAbility  -b  ohos.example.myapplication
+hdc_std hilog -b D -D 00000>./log.txt
