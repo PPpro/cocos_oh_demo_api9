ea970ea073056c5c8fb9113af4e00ce9cfaa3055
PP_Pro <chinese1236@qq.com>
2022-08-03 18:00:15 +0800

    ban log

--- install_run.bat
+++ install_run.bat
@@ -4,4 +4,4 @@ hdc_std uninstall ohos.example.myapplication
 hdc_std install -r %pack_local_path%
 
 hdc_std shell aa start -a com.example.entry.MainAbility  -b  ohos.example.myapplication
-hdc_std hilog -b D -D 00000>./log.txt
+:hdc_std hilog -b D -D 00000>./log.txt
