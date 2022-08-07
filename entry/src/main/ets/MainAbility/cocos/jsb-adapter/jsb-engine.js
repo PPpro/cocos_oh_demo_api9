04dd8b1ee745bdee85516e4090df3327b098596d
PP_Pro <chinese1236@qq.com>
2022-08-07 21:10:03 +0800

    HACK: loadModule

--- entry/src/main/ets/MainAbility/cocos/jsb-adapter/jsb-engine.js
+++ entry/src/main/ets/MainAbility/cocos/jsb-adapter/jsb-engine.js
@@ -1,3 +1,4 @@
+import { loadModule } from '../loadModule';
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
 
@@ -2266,7 +2267,7 @@ function downloadScript(url, options, onComplete) {
 
   if (loadedScripts[url]) return onComplete && onComplete();
   download(url, function (src, options, onComplete) {
-    window.require(src);
+    loadModule(src);
 
     loadedScripts[url] = true;
     onComplete && onComplete(null);
--- entry/src/main/ets/MainAbility/cocos/loadModule.ts
+++ entry/src/main/ets/MainAbility/cocos/loadModule.ts
@@ -4,7 +4,7 @@ const commonJSModuleMap: Record<string, Function> = {
     '/src/application.79b93.js' () { require('./src/application.79b93.js'); },
     '/src/cocos-js/cc.js' () { require('./src/cocos-js/cc.js'); },
     '/src/chunks/bundle.js' () { require('./src/chunks/bundle.js') },
-    'workers/assets/main/index.js' () { require('./assets/main/index.js'); },
+    'assets/main/index.js' () { require('./assets/main/index.js'); },
     '/src/cocos-js/wait-for-ammo-instantiation.js' () { require('./src/cocos-js/wait-for-ammo-instantiation.js'); },
     '/src/cocos-js/instantiated-687fb370.js' () { require('./src/cocos-js/instantiated-687fb370.js'); }
 }
